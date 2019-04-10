5bbd2fb6f39d9ae70194e3614edab88de6c91eda
wanderson <wanderson.alves.rodrigues@gmail.com>
2019-04-10 15:59:32 -0300

    Ajuste cors

--- src/server.js
+++ src/server.js
@@ -4,22 +4,28 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}));
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-io.on('connection', socket =>{
-   socket.on('connectRoom', box =>{
-       socket.join(box);
-   });
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    });
 });
 
 mongoose.connect('mongodb+srv://wanderson:root@cluster0-iaxyk.mongodb.net/omnistack?retryWrites=true', {
     useNewUrlParser: true
 });
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     req.io = io;
     return next();
 });
@@ -28,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-app.use('/files', express.static(path.resolve(__dirname,'.','temp')));
+app.use('/files', express.static(path.resolve(__dirname, '.', 'temp')));
 app.use(require('./routes'));
 
 server.listen(process.env.PORT || 3333);
