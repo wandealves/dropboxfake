92b2b2917738464c566cb0c612419b4f6e58b499
wanderson <wanderson.alves.rodrigues@gmail.com>
2019-04-10 16:05:00 -0300

    Ajuste server

--- src/server.js
+++ src/server.js
@@ -4,13 +4,20 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+/*app.use(cors({
     'allowedHeaders': ['sessionId', 'Content-Type'],
     'exposedHeaders': ['sessionId'],
     'origin': '*',
     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
     'preflightContinue': false
-}));
+}));*/
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
